abad523c90b339a908fee4f16cd3dc5a4ab3e1ac
CM <plusonecm@gmail.com>
2019-10-14 10:51:14 +0800

    fix security issue

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import { WEATHER_API_KEY } from '../config'
 
 export const getWeather = async (cityId) => {
   try {
-    const res = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${WEATHER_API_KEY}&units=metric`)
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&APPID=${WEATHER_API_KEY}&units=metric`)
     const json = await res.json()
     return json
   } catch (error) {
